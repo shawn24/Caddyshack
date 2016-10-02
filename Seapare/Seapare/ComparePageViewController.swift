cd602df645e6be5c5e2806e75c2db00772c201f3
Alan Wu <w.yulun@live.ca>
2016-10-01 22:18:59 -0400

    oct 1

---
+++ Seapare/Seapare/ComparePageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ComparePageViewController.swift
+//  Seapare
+//
+//  Created by Yulun Wu on 2016-10-01.
+//  Copyright © 2016 Yuan Yao. All rights reserved.
+//
+
+import UIKit
+
+class ComparePageViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
