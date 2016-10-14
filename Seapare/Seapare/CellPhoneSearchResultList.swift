294c17416532df289be3e1c43fa2d6969be863f8
xiaoyunpang <xiaoyunpang8@hotmail.com>
2016-10-14 03:01:42 -0400

    add compare functions

---
+++ Seapare/Seapare/CellPhoneSearchResultList.swift
@@ -0,0 +1,13 @@
+//
+//  CellPhoneSearchResultList.swift
+//  Seapare
+//
+//  Created by xiaoyunpang on 2016-10-14.
+//  Copyright © 2016 Yuan Yao. All rights reserved.
+//
+
+import Foundation
+
+class CellPhoneSearchResultsList : NSArray {
+    
+}
