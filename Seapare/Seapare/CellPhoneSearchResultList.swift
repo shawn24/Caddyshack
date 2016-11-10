fc3872a3eb68b2a1e73ac2d70d37b5f997dfb2f9
xiaoyunpang <xiaoyunpang8@hotmail.com>
2016-11-10 16:37:01 -0500

    add sort functions

--- Seapare/Seapare/CellPhoneSearchResultList.swift
+++ Seapare/Seapare/CellPhoneSearchResultList.swift
@@ -32,4 +32,54 @@ class CellPhoneSearchResultsList {
         return sortByName(left) + equal + sortByName(right)
     }
     
+    func sortByBrand(_ cellList : [CellPhone]) -> [CellPhone]{
+        guard cellList.count > 1 else { return cellList }
+        
+        let pivot = cellList[cellList.count/2]
+        let left = cellList.filter { ($0).brand_obj.brand_name < pivot.brand_obj.brand_name }
+        let equal = cellList.filter { ($0).brand_obj.brand_name == pivot.brand_obj.brand_name }
+        let right = cellList.filter { ($0).brand_obj.brand_name > pivot.brand_obj.brand_name }
+        return sortByBrand(left) + equal + sortByBrand(right)
+    }
+    
+    func sortByPrice(_ cellList : [CellPhone]) -> [CellPhone]{
+        guard cellList.count > 1 else { return cellList }
+        
+        let pivot = cellList[cellList.count/2]
+        let left = cellList.filter { ($0).price < pivot.price }
+        let equal = cellList.filter { ($0).price == pivot.price }
+        let right = cellList.filter { ($0).price > pivot.price }
+        return sortByPrice(left) + equal + sortByPrice(right)
+    }
+    
+    func sortByCapacity(_ cellList : [CellPhone]) -> [CellPhone]{
+        guard cellList.count > 1 else { return cellList }
+        
+        let pivot = cellList[cellList.count/2]
+        let left = cellList.filter { ($0).capacity < pivot.capacity }
+        let equal = cellList.filter { ($0).capacity == pivot.capacity }
+        let right = cellList.filter { ($0).capacity > pivot.capacity }
+        return sortByCapacity(left) + equal + sortByCapacity(right)
+    }
+    
+    func sortByRam(_ cellList : [CellPhone]) -> [CellPhone]{
+        guard cellList.count > 1 else { return cellList }
+        
+        let pivot = cellList[cellList.count/2]
+        let left = cellList.filter { ($0).ram < pivot.ram }
+        let equal = cellList.filter { ($0).ram == pivot.ram }
+        let right = cellList.filter { ($0).ram > pivot.ram }
+        return sortByRam(left) + equal + sortByRam(right)
+    }
+    
+    func sortByCamera(_ cellList : [CellPhone]) -> [CellPhone]{
+        guard cellList.count > 1 else { return cellList }
+        
+        let pivot = cellList[cellList.count/2]
+        let left = cellList.filter { ($0).camera_resolution < pivot.camera_resolution }
+        let equal = cellList.filter { ($0).camera_resolution == pivot.camera_resolution }
+        let right = cellList.filter { ($0).camera_resolution > pivot.camera_resolution }
+        return sortByCamera(left) + equal + sortByCamera(right)
+    }
+    
 }
